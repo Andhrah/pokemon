1b7134370d5b6ccb5ef5650a58f1029b0b495baf
Alexandra Collins <andracolexa@gmail.com>
2022-02-20 23:43:13 +0100

    pokecard
    - implement pokecard component
    - create pokecard style file

---
+++ src/Pokecard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Pokecard.css';
+
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+
+class Pokecard extends Component {
+  render() {
+    const {name, id, type, exp} = this.props;
+
+    let imgSrc = `${POKE_API}${id}.png`;
+
+    return (
+      <div className="Pokecard">
+        <h1>{name}</h1>
+        <img src={imgSrc} alt={name} />
+        <div>Type: {type}</div>
+        <div>EXP: {exp}</div>
+      </div>
+    )
+  }
+}
+
+export default Pokecard;
+ 
